a5933d8b21a6e1c877a338b7d9785d88737d3d8a
vankhoazz <lekhoakhoakhanh@gmail.com>
2025-05-18 00:16:37 +0700

    Add files via upload

--- quanlybenxe.cpp
+++ quanlybenxe.cpp
@@ -9,6 +9,7 @@
 #include<cstring>
 #include<algorithm>
 #include <cstdlib>
+#include <limits> 	
 
 using namespace std;
 class Xe {
@@ -27,11 +28,12 @@ public:
 
 	}
 	virtual void xuat() {
-		cout << "Phuong tien: " << loai();
-		cout << "Bien so: " << bienso << ", Tai xe: " << taixe;
+		cout << left << setw(8)<< loai() << setw(8) << bienso << setw(8)<< taixe;
 
 	};
-
+	string getbienso() const { return bienso; }
+	string gettaixe() const { return taixe; }
+	void thaydoitaixe(const string& tx) { taixe = tx; }
 	virtual void ghifile(ofstream& of) {
 		of << bienso << "," << taixe << ",";
 	}
@@ -47,16 +49,17 @@ public:
 		this->diemdon = diemdon;
 		this->diemden = diemden;
 	}
-
 	void nhap() {
 		Xe::nhap();
 		cout << "Nhap diem don: "; getline(cin, diemdon);
 		cout << "Nhap diem den: "; getline(cin, diemden);
 		cout << "Nhap so ghe: "; cin >> soghe; cin.ignore(); 
 	}
+	string getdiemdon() const { return diemdon; }
+	string getdiemden() const { return diemden; }
 	void xuat() {
 		Xe::xuat();
-		cout << "So ghe: " << soghe <<"Diem di: "<<diemdon<<"Diem den: "<<diemden<< endl;
+		cout << left << setw(5) << soghe << setw(8) <<diemdon<< setw(8) <<diemden<< endl;
 	}
 	void ghifile(ofstream& of) {
 		of << "Bus" <<",";
@@ -74,14 +77,14 @@ public:
 	Taxi(string bienso, string taixe, double km) : Xe(bienso, taixe) {
 		this->km = km;
 	}
-
 	void nhap() {
 		Xe::nhap();
 		cout << "Nhap so km: "; cin >> km; cin.ignore();
 	}
+	double getkm() const { return km; }
 	void xuat() {
 		Xe::xuat();
-		cout << "So km: " << km << endl; 
+		cout << left << setw(8) << km << endl; 
 	}
 	void ghifile(ofstream& of) {
 		of << "Taxi"<<",";
@@ -137,9 +140,10 @@ public:
 		system("cls");
 	}
 	void xuattatca() {
+		cout<<"+--------------------------------------------------+\n";
 		for (Xe* v : phtien) {
 			v->xuat();
-			cout << "--------------------\n";
+			cout << "-------------------------------------\n";
 		}
 	}
 	void luufile() {
@@ -189,6 +193,84 @@ void docfile(vector<Xe*>& phtien)
 
 	}
 }
+void timkiem(const vector<Xe*>& phtien) 
+{
+	string timkiem1;
+	cout<<"Nhap phuong tien can tim: ";
+	getline(cin,timkiem1);
+	bool tikiem = false;
+	for (Xe* v : phtien)
+	{
+		if(v->loai() == timkiem1) {
+			cout<<"-------------------------\n";
+			cout<<"Tim thay phuong tien can tim thong tin.\n";
+			v->xuat();
+			cout<<"\n";
+			tikiem = true;
+		}
+		else if(v->loai() == timkiem1) {
+			cout<<"-------------------------\n";
+			cout<<"Tim thay phuong tien can tim thong tin.\n";
+			v->xuat();
+			cout<<"\n";
+			tikiem = true;
+		}
+	}
+	if(!tikiem) {
+		cout<<"Khong tim thay thong tim xe\n";
+	}
+}
+void xoa(vector<Xe*>& phtien) 
+{
+	string xoa1;
+	cout<<"Nhap phuong tien can xoa: ";
+	getline(cin,xoa1);
+	vector<Xe*> phtien1;
+	bool xoa = false;
+	for(Xe* v : phtien) 
+	{
+		if(v->loai() != xoa1)
+		{
+			phtien1.push_back(v);
+		}
+		else {
+			xoa = true;
+		}
+	}
+	if(!xoa)
+	{
+		cout<<"K tim thay thong tim xe can xoa.";
+	}
+	else 
+	{
+		phtien = phtien1;
+		cout<<"Da xoa phuong tien.";
+	}
+
+}
+void suataixe(vector<Xe*>& phtien)
+{
+	string sua1;
+	cout<<"Nhap ten tai xe can sua: ";
+	getline(cin,sua1);
+	bool sua = false;
+	for(Xe* v : phtien)
+	{
+		if(v->gettaixe() == sua1)
+		{
+			string sua2;
+			cout<<"Sua tanh ten moi: ";
+			getline(cin,sua2);
+			v->thaydoitaixe(sua2);
+			sua = true;
+			cout<<"Da sua thanh cong.";
+		}
+	}
+	if(!sua) {
+		cout<<"Khong tim thay tai xe co ten "<<sua1<<endl;
+	}
+
+}
 int main()
 {
 	Benxe quanly;
@@ -196,7 +278,7 @@ int main()
 	int choice;
 	do {
 		cout << "===== Quan ly ben xe Da Nang =====\n";
-		cout << "1. Them phuong tien\n2. Hien thi tat ca\n3. Luu vao file\n0. Thoat\n\nChon: ";
+		cout << "1. Them phuong tien\n2. Hien thi tat ca\n3. Luu vao file\n4. Tim kiem xe\n5. Xoa phuong tien\n6. Sua thong tin tai xe.\n0. Thoat\nChon: ";
 		cin >> choice; cin.ignore();
 		switch (choice)
 		{
@@ -212,7 +294,19 @@ int main()
 			}
 			cout << "\nDa luu vao file QuanLyXe.txt\n";
 			break;
-			}
+		}
+		case 4: {
+			timkiem(quanly.phtien);
+			break;
+		}
+		case 5: {
+			xoa(quanly.phtien);
+			break;
+		}
+		case 6: {
+			suataixe(quanly.phtien);
+			break;
+		}
 		default :
 			cout<<"Lua chon ko hop le moi nhap lai.\n\n";
 		}
